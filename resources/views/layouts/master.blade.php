298b328c4ee403b0bac24a89cd8500971f2c0d6c
Robert Kerr <rksugarfree@gmail.com>
2016-09-24 15:36:21 -0500

    reconfigured index, created blogpost, message, project models, added pixelcog parallax, some images, some routes, created header and js partials

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+
+</head>
+<body>
+@include('partials.header')
+@yield('content')
+@include('partials.js')
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<nav style="background-color: black;" class="navbar navbar-fixed-top">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12 nav_container" id="header_nav">
+                <ul class="social-network social-circle">
+                    <!--                        <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>-->
+                    <!-- <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li> -->
+                    <li><a href="https://www.twitter.com/rksugarfree" target="_blank" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                    <!-- <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li> -->
+                    <li><a href="https://www.linkedin.com/in/robertcrawleykerr" target="_blank" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+                </ul>
+                <ul class="nav nav-pills" id="nav">
+                    <li role="presentation" class="navbutton" id="robertkerr"><a class="listItem {{ $homeNav or "" }}" href="/index">Robert Kerr</a></li>
+                    <li role="presentation" class="navbutton blogProjects"><a class="listItem {{ $projectsNav or "" }}" href="/projects">Projects</a></li>
+                    <li role="presentation" class="navbutton" id="contact"><a class="listItem {{ $blogNav or "" }}" href="/blog">Blog</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</nav>
