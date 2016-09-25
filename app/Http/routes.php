275f60f822b92761fd50c719b06bb99afa4cf578
Robert Kerr <rksugarfree@gmail.com>
2016-09-25 03:40:56 -0500

    created blog placeholder and added route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,5 +14,6 @@
 Route::get('/', 'HomeController@index');
 Route::resource('contact', 'MessageController');
 Route::get('/projects', 'HomeController@projects');
-Route::get('/contact', 'HomeController@contact');
+//Route::get('/contact', 'HomeController@contact');
 Route::get('/message', 'HomeController@message');
+Route::get('/blog', 'BlogController@index');
